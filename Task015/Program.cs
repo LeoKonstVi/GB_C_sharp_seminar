20b38809a041fe90acc8e869acf25dd88d913e8f
Konstantin Leontiev <kleontiev@yandex.ru>
2023-06-02 12:47:16 +0300

    Задача 15 - День недели по цифре

---
+++ Task015/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите день недели (число от 1 до 7):");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+if ( 1 <= dayWeek && dayWeek <= 7 )
+{
+    if (dayWeek == 6 || dayWeek == 7)
+    {
+    Console.WriteLine("Это выходной день");
+    }
+    else
+    {
+    Console.WriteLine("Это рабочий день");    
+    }
+}
+else
+{
+    Console.WriteLine("Неверный ввод");
+}
