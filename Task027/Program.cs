b3984879e3ce78cd33cc3ec9f0bc91e674d8ded6
Konstantin Leontiev <kleontiev@yandex.ru>
2023-06-02 12:52:48 +0300

    Задача 27 - сумма цифр введённого числа

---
+++ Task027/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Функция суммы цифр числа
+
+int SumDigitOfNumber(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum =  SumDigitOfNumber(number);
+
+Console.Write($"Сумма цифр числа {number} равна {sum}");
