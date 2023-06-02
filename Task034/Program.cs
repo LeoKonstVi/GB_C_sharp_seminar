032cd75c1bc7ed1f3a5e3ff49a626118a1e25a37
Konstantin Leontiev <kleontiev@yandex.ru>
2023-06-02 20:04:53 +0300

    Задача 34 - Массив трёхзначных чисел. Количество чётных

---
+++ Task034/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateFillArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+void PrintArray(int[] arr)
+{
+    int length = arr.Length;
+    Console.Write("[");
+    for (int i = 0; i < length; i++)
+    {
+        if (length - i > 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = CreateFillArray(10, 100, 999);
+int countEvenNumbers = CountEvenNumbers(array);
+
+Console.WriteLine("Массив трёхзначных чисел:");
+PrintArray(array);
+Console.WriteLine("Количество чётных чисел:");
+Console.WriteLine(countEvenNumbers);
