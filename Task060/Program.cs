5fbcb8cb96119d3a8d6d0c02432774ef34f99b9b
Konstantin Leontiev <kleontiev@yandex.ru>
2023-06-19 19:20:31 +0300

    Задача 60- Поэлементный вывод 3-х мерного массива двузначных чисел с индексами элементов

--- Task060/Program.cs
+++ Task060/Program.cs
@@ -5,19 +5,27 @@
 // 66(0,0,0) 25(0,1,0) 27(0,0,1) 90(0,1,1)
 // 34(1,0,0) 41(1,1,0) 26(1,0,1) 55(1,1,1)
 
+int RequestNumberInt(string text)
+{
+    Console.Write(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
 // Функция создания 3-х мерного массива заполненного неповторяющимися двузначными числами.
 int[,,] CreateArray3dTwoDigitNumbers(int size1, int size2, int size3)
 {
-    int number = 10; // переменная двузначных чисел
     int[,,] array3d = new int[size1, size2, size3];
+    int twoDigitNumber = 10; // переменная двузначных чисел
+    
     for (int i = 0; i < size1; i++)
     {
         for (int j = 0; j < size2; j++)
         {
             for (int k = 0; k < size3; k++)
             {
-                array3d[i, j, k] = number;
-                number++;
+                array3d[i, j, k] = twoDigitNumber;
+                twoDigitNumber++;
             }
         }
     }
@@ -40,7 +48,18 @@ void PrintArray3d(int[,,] array3d)
     }
 }
 
-int[,,] userArray3d = CreateArray3dTwoDigitNumbers(4, 3, 3);
+Console.WriteLine("Введите размеры 3-х мерного массива для заполнения двузначными числами");
+int size1 = RequestNumberInt("Введите размер один: ");
+int size2 = RequestNumberInt("Введите размер два: ");
+int size3 = RequestNumberInt("Введите размер три: ");
+int length = size1 * size2 * size3;
+if (length > 90)
+    {
+        Console.WriteLine($"Размер массива {length} превышает 90 количество двузначных чисел");
+        return;
+    }
+
+int[,,] userArray3d = CreateArray3dTwoDigitNumbers(size1, size2, size3);
 Console.WriteLine();
 PrintArray3d(userArray3d);
 Console.WriteLine();
