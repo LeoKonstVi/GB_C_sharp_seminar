a4c9624a7cf3bc7e3962ffb041291c2095e1a807
Konstantin Leontiev <kleontiev@yandex.ru>
2023-06-19 20:23:08 +0300

    Задача 64 - Рекурсия. Вывод натуральных чисел от заданного N

---
+++ Task064/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void NaturalNumber(int n)
+{
+    if (n == 1)
+    {
+        Console.Write(1);
+        return;
+    }
+    Console.Write($"{n}, ");
+    NaturalNumber(n - 1);
+}
+
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0)
+{
+    Console.WriteLine("Введено ненатуральное число!");
+    return;
+}
+Console.WriteLine();
+Console.WriteLine("Натуральные числа:");
+NaturalNumber(number);
+Console.WriteLine();
+Console.WriteLine();
